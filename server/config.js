0a9b094dde35704a0d64482612cf03339d65b46c
Alejandro Barazarte <abx.com@gmail.com>
2015-09-04 12:20:52 -0430

    Login and Signup Express Routes

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  db: process.env.db || 'localhost',
+  db: process.env.db || 'mongodb://testuser:testuser@ds035673.mongolab.com:35673/ab-test',
   tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
 };
--- server/server.js
+++ server/server.js
@@ -26,8 +26,8 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(cors());
-app.use(bodyParser().json());
-app.use(bodyParser().urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 function createToken(user){
@@ -62,6 +62,50 @@ function isAuthenticated(req, res, next){
     next();
   })
 }
+
+app.post('/auth/login', function(req, res){
+  User.findOne({ email: req.body.email }, '+password', function(err, user){
+    if(!user){
+      return res.status(401).send({ message: { email: 'Incorrect email' } });
+    }
+
+    bcrypt.compare(req.body.password, user.password, function(err, isMatch){
+      if(!isMatch){
+        return res.status(401).send({ message: { password: 'Incorrect password' } });
+      }
+
+      user = user.toObject();
+      delete user.password;
+
+      var token = createToken(user);
+      res.send({ token: token, user: user });
+    });
+  });
+});
+
+app.post('/auth/signup', function(req, res){
+  User.findOne({ email: req.body.email }, function(err, existingUser){
+    if(existingUser){
+      return res.status(409).send({ message: 'Email is already taken.' });
+    }
+
+    var user = new User({
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(user.password, salt, function(err, hash){
+        user.password = hash;
+
+        user.save(function(){
+          var token = createToken(user);
+          res.send({ token: token, user: user });
+        });
+      });
+    });
+  });
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
