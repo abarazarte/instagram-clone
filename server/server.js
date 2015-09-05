1f8dfd8a923a0b25a29d495581a2778c3498dd3f
Alejandro Barazarte <abx.com@gmail.com>
2015-09-05 16:22:23 -0430

    Optimizations

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var moment = require('moment');
 var mongoose = require('mongoose');
 var path = require('path');
 var request = require('request');
+var compress = require('compression');
 
 var config = require('./config');
 
@@ -45,7 +46,7 @@ app.use(function(req, res, next){
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2628000000 }));
 
 function createToken(user){
   var payload = {
