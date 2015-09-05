ac4d9ade7936bd895a56219ccb5e09fbb0769824
Alejandro Barazarte <abx.com@gmail.com>
2015-09-05 14:35:50 -0430

    Added Instagram API Endpoints

--- server/server.js
+++ server/server.js
@@ -122,8 +122,44 @@ app.post('/auth/instagram', function(req, res){
   request.post({ url: accessTokenUrl, form: params, json: true }, function(e, r, body){
     //Step 2a. Link user accounts.
     if(req.headers.authorization){
+      User.findOne({ instagramId: body.user.id }, function(err, existingUser){
+        var token = req.headers.authorization.split(' ')[1];
+        var payload = jwt.decode(token, config.tokenSecret);
 
-    }else { //Step 2b. Create a new user account or return an existing one.
+        User.findById(payload.sub, '+password', function(err, localUser){
+          if(!localUser){
+            return res.status(400).send({ message: 'User not found.' });
+          }
+
+          //Merge two accounts.
+          if(existingUser){
+            existingUser.email = localUser.email;
+            existingUser.password = localUser.password;
+
+            localUser.remove();
+
+            existingUser.save(function(){
+              var token = createToken(existingUser);
+              return res.send({ token: token, user: existingUser });
+            });
+          } else {
+            //Link current email account with the instagram profile information.
+            localUser.instagramId = body.user.id;
+            localUser.username = body.user.username;
+            localUser.fullName = body.user.full_name;
+            localUser.picture = body.user.profile_picture;
+            localUser.accessToken = body.access_token;
+
+            localUser.save(function(){
+              var token = createToken(localUser);
+              res.send({ token: token, user: localUser });
+            });
+
+          }
+        });
+      });
+    }else {
+      //Step 2b. Create a new user account or return an existing one.
       User.findOne({ instagramId: body.user.id }, function(err, existingUser){
         if(existingUser){
           var token = createToken(existingUser);
@@ -144,6 +180,44 @@ app.post('/auth/instagram', function(req, res){
         });
       });
     }
+  });
+});
+
+app.get('/api/feed', isAuthenticated, function(req, res){
+  var feedUrl = 'https://api.instagram.com/v1/users/self/feed';
+  var params = { access_token: req.user.accessToken };
+
+  request.get({ url: feedUrl, qs: params, json: true }, function(err, res, body){
+    if(!err && res.statusCode == 200){
+      res.send(body.data);
+    }
+  });
+});
+
+app.get('/api/media/:id', isAuthenticated, function(req, res, next){
+  var mediaUrl = 'https://api.instagram.com/v1/media/' + req.params.id;
+  var params = { access_token: req.user.accessToken };
+
+  request.get({ url: mediaUrl, qs: params, json: true }, function(err, res, body){
+    if(!err && res.statusCode == 200){
+      res.send(body.data);
+    }
+  });
+});
+
+app.post('/api/like', isAuthenticated, function(req, res, next){
+  var mediaId = req.body.mediaId;
+  var accessToken = { access_token: req.user.accessToken };
+  var likeUrl = 'https://api.instagram.com/v1/media/' + mediaId + '/likes';
+
+  request.post({ url: likeUrl, form: accessToken, json:true }, function(err, response, body){
+    if(response.statusCode !== 200){
+      return res.statusCode(response.statusCode).send({
+        code: response.statusCode,
+        message: body.meta.error_message
+      });
+    }
+    res.status(200).end();
   });
 });
 
